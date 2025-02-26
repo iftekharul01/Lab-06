e615d2f15b4dbd95e5b2a788e83427c89001b703
iftekharul01 <iftekharulislam001@gmail.com>
2025-02-26 10:57:47 +0600

    drawer

--- lab_6/lib/main.dart
+++ lab_6/lib/main.dart
@@ -12,6 +12,36 @@ class lab06 extends StatelessWidget {
     return MaterialApp(
       title: 'Lab 06',
       home: Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Profile'),
+                trailing: Icon(Icons.arrow_drop_down),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.alarm),
+                title: Text('Recent'),
+                //trailing: Icon(Icons.arrow_drop_down),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.image),
+                title: Text('Images'),
+                //trailing: Icon(Icons.arrow_drop_down),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.play_circle_outline),
+                title: Text('Video'),
+                //trailing: Icon(Icons.arrow_drop_down),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
         appBar: AppBar(
           centerTitle: true,
           title: const Text('Lab 06'),
